ed3059fe6d60ab7b28703009085a2e025a3bdd5f
Wesley Halstead <wesley.halstead@tamu.edu>
2025-10-17 15:25:10 -0500

    Implement compatibility checks for fitLASSOstandardized

--- LassoFunctions.R
+++ LassoFunctions.R
@@ -47,12 +47,21 @@ lasso <- function(Xtilde, Ytilde, beta, lambda){
 # eps - precision level for convergence assessment, default 0.001
 fitLASSOstandardized <- function(Xtilde, Ytilde, lambda, beta_start = NULL, eps = 0.001){
   #[ToDo]  Check that n is the same between Xtilde and Ytilde
-  
+  if (nrow(Xtilde) != length(Ytilde)) {
+    stop("Xtilde must have number of rows equal to the length of Ytilde")
+  }
   #[ToDo]  Check that lambda is non-negative
-  
+  if (lambda < 0) {
+    stop("Lambda must be non-negative")
+  }
   #[ToDo]  Check for starting point beta_start. 
   # If none supplied, initialize with a vector of zeros.
   # If supplied, check for compatibility with Xtilde in terms of p
+  if (is.null(beta_start)){
+    beta_start = numeric(ncol(Xtilde))
+  } else if (ncol(Xtilde) != length(beta_start)) {
+    stop("beta_start must have length equal to the number of rows in Xtilde")
+  }
   
   #[ToDo]  Coordinate-descent implementation. 
   # Stop when the difference between objective functions is less than eps for the first time.
