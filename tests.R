5eb79a7b664f92b6d0fa334fc36e283a4198efb9
Wesley Halstead <wesley.halstead@tamu.edu>
2025-10-17 20:23:24 -0500

    Add tests for softmax function

--- tests.R
+++ tests.R
@@ -1,6 +1,7 @@
 library(testthat)
 source("LassoFunctions.R")
 
+# Test standardizing function
 set.seed(100)
 X <- matrix(rnorm(50 * 50, 10, 3), 50, 50)
 Y <- rnorm(50, 15, 2)
@@ -12,3 +13,13 @@ test_that(
   expect_equal(colMeans(out$Xtilde), numeric(50))
   expect_equal(mean(out$Ytilde), 0)
 })
+
+
+# Test softmax function
+test_that(
+  "Softmax function works properly", {
+    expect_equal(soft(5, 2), 3)
+    expect_equal(soft(-5, 2), -3)
+    expect_equal(soft(-5, 7), 0)
+    expect_equal(soft(5, 7), 0)
+  })
